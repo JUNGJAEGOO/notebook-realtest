589fadefd3890ab46c0e41645be8934ed8e2e8bf
JUNGJAEGOO <worn1215@naver.com>
2017-12-20 22:36:56 +0900

    171220

--- back1699/src/back1699/Main.java
+++ back1699/src/back1699/Main.java
@@ -5,25 +5,34 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
-		int N = in.nextInt();
-		int dp[] = new int[N+1];
-		for(int i = 1 ; i*i <= N ; i++) {
-			int tmp = i*i;
-			dp[tmp] = 1;
-			
+		int N  = in.nextInt();   // 100
+		int dp[] = new int[100001];
+		int sq[] = new int[317];
+		
+		for (int i = 1 ; i <= 316 ; i++) {
+			sq[i] = i*i;
+			//dp[i*i] = 1;
 		}
 		
-	    for (int i = 1; i <= N; i++)
-	    {
-	        for (int j = 1; j*j <= i; j++){
-	            if (dp[i] > dp[i - j*j] + 1 || dp[i] == 0)
-	                dp[i] = dp[i - j*j] + 1;
-	        }
-	    }
-
-
-		for(int i =1;i<=N;i++) {
-			System.out.println(i+" "+dp[i]);
+		for ( int j = 1 ; j <= 316 ; j++) {
+			for (int i = sq[j] ; i <= 100000 ; i++) {
+				if ( dp[i] != 0) {
+					dp[i] = Math.min(dp[i-sq[j]]+1,dp[i]);
+				}else {
+					dp[i] = dp[i-sq[j]]+1;
+				}
+			}
 		}
+		
+		
+		/*for ( int i = 1 ; i <= 10 ;i++) {
+			for ( int j = 1 ; j <= 10; j++) {
+				System.out.print(dp[10*(i-1)+j]+" ");
+			}System.out.println();
+		}*/
+		System.out.println(dp[N]);
+		
 	}
+	
+
 }
